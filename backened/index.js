3b2001f22661e83acdc91b3ed05ac8a7a1404b68
RishabhSkr <iamrishab786@gmail.com>
2025-01-09 07:41:44 +0530

    fronetened uploaded on render

--- backened/index.js
+++ backened/index.js
@@ -4,7 +4,9 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import stockRoutes from './routes/stockRoutes.js';
 import { errorHandler } from './middleware/errorHandler.js';
-dotenv.config();
+dotenv.config(
+    path = "./.env"
+);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,10 +15,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 // CORS configuration
 app.use(cors({
-    origin: ['http://localhost:5173',"https://portfolio-tracker-capx.vercel.app"],
+    origin: ['http://localhost:5173',"https://portfolio-tracker-capx.vercel.app", process.env.CLIENT_URL],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
-    credentials: true
 }));
 
 
