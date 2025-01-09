c645aa708d3e84151f8fc3a5a01cd11e059a7a33
RishabhSkr <iamrishab786@gmail.com>
2025-01-09 07:43:47 +0530

    path undefinged solved

--- backened/index.js
+++ backened/index.js
@@ -4,9 +4,7 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import stockRoutes from './routes/stockRoutes.js';
 import { errorHandler } from './middleware/errorHandler.js';
-dotenv.config(
-    path = "./.env"
-);
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
